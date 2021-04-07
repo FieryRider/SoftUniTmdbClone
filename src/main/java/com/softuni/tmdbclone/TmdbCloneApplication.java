4161f4182d9f138b3de9059350b0fa778ebf71ec
FieryRider <30357809+FieryRider@users.noreply.github.com>
2021-04-07 18:10:21 +0300

    Init

---
+++ src/main/java/com/softuni/tmdbclone/TmdbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.tmdbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmdbCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softuni/tmdbclone/TmdbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.tmdbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
