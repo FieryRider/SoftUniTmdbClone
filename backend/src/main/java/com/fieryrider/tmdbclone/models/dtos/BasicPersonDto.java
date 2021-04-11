6bb2f81855f7fcacd752bbe21e1fed2b07a1a69b
FieryRider <30357809+FieryRider@users.noreply.github.com>
2021-04-11 07:32:15 +0300

    Fix BasicPersonDto not including 'id'

--- backend/src/main/java/com/fieryrider/tmdbclone/models/dtos/BasicPersonDto.java
+++ backend/src/main/java/com/fieryrider/tmdbclone/models/dtos/BasicPersonDto.java
@@ -3,8 +3,19 @@ package com.fieryrider.tmdbclone.models.dtos;
 import java.util.List;
 
 public class BasicPersonDto {
+    private String id;
+
     private String name;
+
     private List<String> knownFor;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return this.name;
